06ad9afa3270381a0396d409c437d69cd530f8a4
windmetaru <windmetaru@mail.com>
2016-02-28 09:23:31 -0300

    Update army.py

--- army.py
+++ army.py
@@ -2,11 +2,27 @@
 import random
 
 class Army(object):
-	def __init__(self, size):
-		self.size = size
+	def __init__(self, lightInfantry, heavyInfantry, archers, lightCavalry, heavyCavalry, pikemen):
+		self.lightInfantry = lightInfantry
+		self.heavyInfantry = heavyInfantry
+		self.archers = archers
+		self.lightCavalry = lightCavalry
+		self.heavyCavalry = heavyCavalry
+		self.pikemen = pikemen
 	
+		
 	def __str__(self):
-		return "size = ({0})".format(self.size)
+		return """
+Light Infantry = ({0})
+Heavy Infantry = ({1})
+Archers        = ({2})
+Light Cavalry  = ({3})
+Heavy Cavalry  = ({4})
+Pikemen        = ({5})
+		""".format(
+		self.lightInfantry, self.heavyInfantry, self.archers,
+		self.lightCavalry, self.heavyCavalry, self.pikemen
+		)
 		
 # In order, Light Inf, Heavy Inf, Archers, Light Cav, Heavy Cav, Pikemen
 
@@ -23,3 +39,7 @@ armyTypes["Light Infantry"], armyTypes["Heavy Infantry"], armyTypes["Archers"],
 armyTypes["Light Cavalry"], armyTypes["Heavy Cavalry"], armyTypes["Pikemen"]))
 
 print("TOTAL: {0}".format(sum(armyTypes.values())))
+
+print(Army(armyTypes["Light Infantry"], armyTypes["Heavy Infantry"], armyTypes["Archers"],
+	 armyTypes["Light Cavalry"], armyTypes["Heavy Cavalry"], armyTypes["Pikemen"]
+	 ))
